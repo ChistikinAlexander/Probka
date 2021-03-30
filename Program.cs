edec69858bdf543064e459a07e090ca6e22685cd
Александр <sandro.gmail@mail.ru>
2021-03-30 12:39:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.ForegroundColor = ConsoleColor.Black;
+            Random rand = new Random();
+            int n = 300;
+            char[,] a = new char[300, 300];
+            while (true)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        a[i, j] = (char)rand.Next(0x0410, 0x44F);
+                        Console.Write(a[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
